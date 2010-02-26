5c5b3e60ebfd1e0347c537c86e43fa7b4850b1ae
Scott Taylor <scott@railsnewbie.com>
2010-02-25 21:01:23 -0500

    Update node.js spec helper

--- spec/node.js
+++ spec/node.js
@@ -1,26 +1,9 @@
-process.mixin(require('sys'));
-require.paths.unshift('spec', '/Users/scotttaylor/.rvm/gems/ruby/1.8.6/gems/jspec-3.2.1/lib', 'lib');
+require.paths.unshift('spec', '/Users/scotttaylor/.rvm/gems/ruby/1.8.6/gems/jspec-3.3.2/lib', 'lib');
 require('jspec');
 require('unit/spec.helper');
-require('node_debugger');
-
-quit = process.exit;
-print = puts;
-
-readFile = function(path) {
-  var result;
-  require('posix')
-    .cat(path, "utf8")
-    .addCallback(function(contents){ result = contents; })
-    .addErrback(function(){ throw new Error("failed to read file `" + path + "'"); })
-    .wait();
-  return result;
-};
+require('yourlib');
 
 JSpec
   .exec('spec/unit/spec.js')
-  .run({
-    reporter: JSpec.reporters.Terminal,
-    fixturePath: 'spec/fixtures'
-  })
+  .run({ reporter: JSpec.reporters.Terminal, fixturePath: 'spec/fixtures' })
   .report();
