c2ebf8a524473610acacba11c5c4afb94c3511f5
Scott Taylor <scott@railsnewbie.com>
2010-02-27 17:44:06 -0500

    switch to common js module system

--- spec/node.js
+++ spec/node.js
@@ -1,7 +1,9 @@
 require.paths.unshift('spec', '/Users/scotttaylor/.rvm/gems/ruby/1.8.6/gems/jspec-3.3.2/lib', 'lib');
 require('jspec');
 require('unit/spec.helper');
-require('node_debugger');
+
+require.paths.unshift("./../lib");
+NodeDebugger = require("node_debugger");
 
 JSpec
   .exec('spec/unit/spec.js')
