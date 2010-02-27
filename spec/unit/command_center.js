650152c660ff82621de5d8189a3a23737ab5407e
Scott Taylor <scott@railsnewbie.com>
2010-02-27 04:08:50 -0500

    Add command center listener, main loop

--- spec/unit/command_center.js
+++ spec/unit/command_center.js
@@ -1,7 +1,15 @@
 describe("NodeDebugger", function() {
   describe("CommandCenter", function() {
     before_each(function() {
+      mock_stdio = {
+        open: function() {},
+        addListener: function() {}
+      };
+
       command_center = Object.create(NodeDebugger.CommandCenter);
+      command_center.stdio = mock_stdio;
+      command_center.print = function() {};
+      command_center.commands = Object.create(NodeDebugger.Commands);
     });
 
     it("should output the repl text", function() {
@@ -11,7 +19,7 @@ describe("NodeDebugger", function() {
         text += t;
       };
 
-      command_center.loop();
+      command_center.prompt();
 
       text.search(/ndb\> /).should.not.equal(-1);
     });
@@ -26,11 +34,11 @@ describe("NodeDebugger", function() {
       });
 
       it("should return the raw_write command when matching rw <some text>", function() {
-        command_center.parse("rw {}").should.equal(NodeDebugger.Commands.raw_write);
+        command_center.parse("rw {}").toString().should.equal([NodeDebugger.Commands.raw_write, "{}"].toString());
       });
 
       it("should return the raw_write command when matching any rw <some text>", function() {
-        command_center.parse("rw {foo: bar}").should.equal(NodeDebugger.Commands.raw_write);
+        command_center.parse("rw {foo: bar}").toString().should.equal([NodeDebugger.Commands.raw_write, "{foo: bar}"].toString());
       });
 
       it("should not return the raw_write command when matching rw at the end of the string", function() {
@@ -47,6 +55,72 @@ describe("NodeDebugger", function() {
         command_center.parse("bar");
         out.should.equal("unknown command: bar");
       });
+    });
+
+    describe("loop", function() {
+      it("should set the connection of the commands object", function() {
+        command_center.connection = {};
+
+        command_center.loop();
+        command_center.commands.connection.should.equal(command_center.connection);
+      });
+
+      it('should display the prompt', function() {
+        prompt_displayed = false;
+
+        command_center.prompt = function() {
+          prompt_displayed = true;
+        };
+
+        command_center.loop();
+        prompt_displayed.should.be(true);
+      });
+
+      it('should open stdio', function() {
+        opened = false;
+
+        mock_stdio.open = function() {
+          opened = true;
+        };
+
+        command_center.loop();
+        opened.should.be(true);
+      });
+
+      it("should add the listener", function() {
+        var args = null;
+
+        mock_stdio.addListener = function() {
+          args = Array.prototype.slice.call(arguments);
+        };
+
+        command_center.loop();
+        args.toString().should.equal(["data", command_center.stdinListener].toString());
+      });
+    });
+
+    describe("stdinListener", function() {
+      it("should call the function if parsed correctly", function() {
+        var called = false;
+
+        command_center.commands.raw_write = function() {
+          called = true;
+        };
+
+        command_center.stdinListener("rw {}");
+        called.should.be(true);
+      });
+
+      it("should call the function with arguments", function() {
+        var called_with = undefined;
+
+        command_center.commands.raw_write = function(arg) {
+          called_with = arg;
+        };
+
+        command_center.stdinListener("rw {}");
+        called_with.should.equal("{}");
+      })
     });
   });
 });
