046aea7a00a45e14554c595fae7f67ba4cf54f5c
Scott Taylor <scott@railsnewbie.com>
2010-03-13 18:28:04 -0500

    Turn verbose back on by default (it's too hard to debug without it)

--- spec/unit/ndb.js
+++ spec/unit/ndb.js
@@ -3,5 +3,10 @@ describe("Node Debugger", function() {
     it("should be at 0.0.1", function() {
       ndb.version.should.equal("0.0.1");
     });
+
+    it("should have verbose on by default", function() {
+      ndb.reset();
+      ndb.verbose.should.equal(true);
+    });
   });
 });
