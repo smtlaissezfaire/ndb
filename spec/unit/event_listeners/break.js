dcb062e148f7bd9943559815ecaaad99ef8171f7
Scott Taylor <scott@railsnewbie.com>
2010-03-19 00:58:04 -0400

    Fix reporting of breakpoint line numbers inline (off by one error)

--- spec/unit/event_listeners/break.js
+++ spec/unit/event_listeners/break.js
@@ -17,7 +17,7 @@ describe("NodeDebugger", function() {
       // {"seq":117,"type":"event","event":"break","body":{"functionName":"f","sourceLine":1,"sourceColumn":14}}
 
       it("should output a break event", function() {
-        var json = '{"seq":117,"type":"event","event":"break","body":{"functionName":"f","sourceLine":1,"sourceColumn":14}}';
+        var json = '{"seq":117,"type":"event","event":"break","body":{"functionName":"f","sourceLine":0,"sourceColumn":14}}';
 
         event_listner.receive(SpecHelpers.makeResponse(json));
 
@@ -26,7 +26,7 @@ describe("NodeDebugger", function() {
 
       it("should buffer the data", function() {
         var json = '{"seq":117,"type":"event","event":"break","bo';
-        var json2 = 'dy":{"functionName":"f","sourceLine":1,"sourceColumn":14}}';
+        var json2 = 'dy":{"functionName":"f","sourceLine":0,"sourceColumn":14}}';
 
         event_listner.receive(SpecHelpers.makeHeader(json + json2) + json);
         event_listner.receive(json2);
@@ -35,7 +35,7 @@ describe("NodeDebugger", function() {
       });
 
       it("should reset the buffer after a successful call", function() {
-        var json = '{"seq":117,"type":"event","event":"break","body":{"functionName":"f","sourceLine":1,"sourceColumn":14}}';
+        var json = '{"seq":117,"type":"event","event":"break","body":{"functionName":"f","sourceLine":0,"sourceColumn":14}}';
 
         event_listner.receive(SpecHelpers.makeResponse(json));
 
@@ -43,15 +43,15 @@ describe("NodeDebugger", function() {
       });
 
       it("should use the correct source column number", function() {
-        var json = '{"seq":117,"type":"event","event":"break","body":{"functionName":"f","sourceLine":1,"sourceColumn":6}}';
+        var json = '{"seq":117,"type":"event","event":"break","body":{"functionName":"f","sourceLine":0,"sourceColumn":6}}';
 
         event_listner.receive(SpecHelpers.makeResponse(json));
 
         out.should.equal("Breakpoint at 1:6 (in function f)");
       });
 
-      it("should use the correct source line number", function() {
-        var json = '{"seq":117,"type":"event","event":"break","body":{"functionName":"f","sourceLine":2,"sourceColumn":14}}';
+      it("should use the correct source line number (first line === 0)", function() {
+        var json = '{"seq":117,"type":"event","event":"break","body":{"functionName":"f","sourceLine":1,"sourceColumn":14}}';
 
         event_listner.receive(SpecHelpers.makeResponse(json));
 
@@ -59,7 +59,7 @@ describe("NodeDebugger", function() {
       });
 
       it("should use the correct function name", function() {
-        var json = '{"seq":117,"type":"event","event":"break","body":{"functionName":"foo","sourceLine":1,"sourceColumn":14}}';
+        var json = '{"seq":117,"type":"event","event":"break","body":{"functionName":"foo","sourceLine":0,"sourceColumn":14}}';
 
         event_listner.receive(SpecHelpers.makeResponse(json));
 
@@ -73,7 +73,7 @@ describe("NodeDebugger", function() {
 
         event_listner.receive(SpecHelpers.makeResponse(json));
 
-        out.should.equal("Breakpoint at test.js:12:22 (in function g - breakpoints: [1])");
+        out.should.equal("Breakpoint at test.js:13:22 (in function g - breakpoints: [1])");
       });
 
       // {"seq":117,"type":"event","event":"break","body":{"functionName":"h","sourceLine":100,"sourceColumn":12,"breakpoints":[3,5,7]}}
@@ -82,7 +82,7 @@ describe("NodeDebugger", function() {
 
         event_listner.receive(SpecHelpers.makeResponse(json));
 
-        out.should.equal("Breakpoint at 100:12 (in function h - breakpoints: [3,5,7])");
+        out.should.equal("Breakpoint at 101:12 (in function h - breakpoints: [3,5,7])");
       });
 
       it("should output the sourceline if one is given", function() {
