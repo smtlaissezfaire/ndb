0d48b21d8c81a3225b2a0e50675f060ea8aa4cbb
Scott Taylor <scott@railsnewbie.com>
2010-03-19 04:24:34 -0400

    show two lines of context when listing code:
    this:
    ndb> list
       683   var self = this;
       684   // remove shebang
    => 685   content = content.replace(/^\#\!.*/, '');
       686
       687   // Compile content if needed
    
    instead of this:
    ndb> list
    => 683   var self = this;
       684   // remove shebang
       685   content = content.replace(/^\#\!.*/, '');
       686
       687   // Compile content if needed

--- spec/unit/commands/list.js
+++ spec/unit/commands/list.js
@@ -44,9 +44,50 @@ describe("NodeDebugger", function() {
           list.run();
 
           spy.intercepted(writer, "run", function(obj) {
-            obj["arguments"].fromLine.should.equal(9);
-            obj["arguments"].toLine.should.equal(14);
+            obj["arguments"].fromLine.should.equal(7);
+            obj["arguments"].toLine.should.equal(12);
           });
+        });
+      });
+
+      describe("context", function() {
+        it("should have the default of 2", function() {
+          list.context.should.equal(2);
+        });
+      });
+
+      describe("calculating the context", function() {
+        it("should return -2, +2", function() {
+          var context = list.calculateContext(10);
+          context.posterior.should.equal(8);
+          context.anterior.should.equal(12);
+        });
+
+        it("should use the correct numbers", function() {
+          var context = list.calculateContext(20);
+          context.posterior.should.equal(18);
+          context.anterior.should.equal(22);
+        });
+
+        it("should calculate based on the value of the context variable", function() {
+          list.context = 3;
+
+          var context = list.calculateContext(10);
+          context.posterior.should.equal(7);
+          context.anterior.should.equal(13);
+        });
+
+        it("should never go below 1", function() {
+          var context = list.calculateContext(1);
+          context.posterior.should.equal(1);
+        });
+
+        it("should change the second value to account for the first going below 1", function() {
+          list.context.should.equal(2);
+
+          var context = list.calculateContext(1);
+          context.posterior.should.equal(1);
+          context.anterior.should.equal(5);
         });
       });
     });
