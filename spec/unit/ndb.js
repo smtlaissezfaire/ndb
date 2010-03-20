498b1fe8c3202b82d6f02885238ef287a1fbabb1
Scott Taylor <scott@railsnewbie.com>
2010-03-20 14:35:39 -0400

    Update version to 0.1.0

--- spec/unit/ndb.js
+++ spec/unit/ndb.js
@@ -1,7 +1,7 @@
 describe("Node Debugger", function() {
   describe("version", function() {
-    it("should be at 0.0.1", function() {
-      ndb.version.should.equal("0.0.1");
+    it("should be at 0.1.0", function() {
+      ndb.version.should.equal("0.1.0");
     });
 
     it("should have verbose off by default", function() {
