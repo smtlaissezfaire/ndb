91d07caafdce866388329d082e81c34934109c57
Scott Taylor <scott@railsnewbie.com>
2010-03-27 17:20:52 -0400

    output a breakpoint confirmation

--- spec/unit/commands/setbreakpoint.js
+++ spec/unit/commands/setbreakpoint.js
@@ -59,6 +59,61 @@ describe("NodeDebugger", function() {
         breakpoint.run();
         obj.arguments.target.should.equal(null);
       });
+
+      // The result of the setbreakpoint request is a response with the number of the newly created break point. This break point number is used in the changebreakpoint and clearbreakpoint requests.
+      //
+      // { "seq"         : <number>,
+      //   "type"        : "response",
+      //   "request_seq" : <number>,
+      //   "command"     : "setbreakpoint",
+      //   "body"        : { "type"       : <"function" or "script">
+      //                     "breakpoint" : <break point number of the new break point>
+      //                   }
+      //   "running"     : <is the VM running after sending this response>
+      //   "success"     : true
+      // }
+      // Here are a couple of examples.
+
+      describe("receiving a breakpoint confirmation", function() {
+        before_each(function() {
+          msg = {
+            type: "response",
+            request_seq: 1,
+            command: "setbreakpoint",
+            body: {
+              type: "script",
+              breakpoint: 1
+            },
+            running: false,
+            success: true
+          };
+
+          event_listener = ndb.EventListener;
+          ndb.verbose = false;
+        });
+
+        it("should output the setbreakpoint confirmation", function() {
+          spy.spyOn(ndb.Helpers, function() {
+            event_listener.receive(SpecHelpers.makeResponse(JSON.stringify(msg)));
+
+            spy.intercepted(ndb.Helpers, "puts", function(str) {
+              str.should.equal("Breakpoint 1 set");
+            });
+          });
+        });
+
+        it("should output the correct breakpoint number", function() {
+          msg.body.breakpoint = 2;
+
+          spy.spyOn(ndb.Helpers, function() {
+            event_listener.receive(SpecHelpers.makeResponse(JSON.stringify(msg)));
+
+            spy.intercepted(ndb.Helpers, "puts", function(str) {
+              str.should.equal("Breakpoint 2 set");
+            });
+          });
+        });
+      });
     });
   });
 });
