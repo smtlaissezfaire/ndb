90793f45a0f58e9016ed918d7c2f92953dac814d
Scott Taylor <scott@railsnewbie.com>
2010-03-27 20:19:35 -0400

    Display the file & linno when setting a breakpoint

--- spec/unit/commands/setbreakpoint.js
+++ spec/unit/commands/setbreakpoint.js
@@ -113,6 +113,52 @@ describe("NodeDebugger", function() {
             });
           });
         });
+
+        // ndb> break 10
+        // verbose: >>> Content-Length: 140
+        // verbose: >>>
+        // verbose: >>> {"type":"request","command":"setbreakpoint","arguments":{"type":"script","target":"/Users/scotttaylor/src/git/ndb/spec/node.js","line":10}}
+        // verbose: >>>
+        // verbose: <<< Content-Length: 222
+        // verbose: <<<
+        // verbose: <<< {"seq":3,"type":"response","command":"setbreakpoint","success":true,"body":{"type":"scriptName","breakpoint":1,"script_name":"/Users/scotttaylor/src/git/ndb/spec/node.js","line":10,"column":null},"refs":[],"running":false}
+        it("should output the script_name and line number when given", function() {
+          msg.body.script_name = "/Users/scotttaylor/src/git/ndb/spec/node.js";
+          msg.body.line        = 10;
+
+          spy.spyOn(ndb.Helpers, function() {
+            event_listener.receive(SpecHelpers.makeResponse(JSON.stringify(msg)));
+
+            spy.intercepted(ndb.Helpers, "puts", function(str) {
+              str.should.equal("Breakpoint 1 set at /Users/scotttaylor/src/git/ndb/spec/node.js:10");
+            });
+          });
+        });
+
+        it("should use the correct file + line no.", function() {
+          msg.body.script_name = "/foo.js";
+          msg.body.line        = 20;
+
+          spy.spyOn(ndb.Helpers, function() {
+            event_listener.receive(SpecHelpers.makeResponse(JSON.stringify(msg)));
+
+            spy.intercepted(ndb.Helpers, "puts", function(str) {
+              str.should.equal("Breakpoint 1 set at /foo.js:20");
+            });
+          });
+        });
+
+        it("should output the filename without a line number if no lineno is given", function() {
+          msg.body.script_name = "/foo.js";
+
+          spy.spyOn(ndb.Helpers, function() {
+            event_listener.receive(SpecHelpers.makeResponse(JSON.stringify(msg)));
+
+            spy.intercepted(ndb.Helpers, "puts", function(str) {
+              str.should.equal("Breakpoint 1 set at /foo.js");
+            });
+          });
+        });
       });
     });
   });
