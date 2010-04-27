aa84833a4d86bd3f64b559b9cf81967d0b4654c2
Scott Taylor <scott@railsnewbie.com>
2010-04-27 03:54:59 -0400

    release ndb 0.1.2

--- spec/unit/ndb.js
+++ spec/unit/ndb.js
@@ -1,7 +1,7 @@
 describe("Node Debugger", function() {
   describe("version", function() {
-    it("should be at 0.1.0", function() {
-      ndb.version.should.equal("0.1.0");
+    it("should be at 0.1.2", function() {
+      ndb.version.should.equal("0.1.2");
     });
 
     it("should have verbose off by default", function() {
