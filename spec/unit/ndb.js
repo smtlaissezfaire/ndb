9a15cc918c39843d953b4212942ad222a5e2f4ce
Scott Taylor <scott@railsnewbie.com>
2010-05-16 15:09:46 -0400

    cleaner scoping of specs

--- spec/unit/ndb.js
+++ spec/unit/ndb.js
@@ -3,10 +3,10 @@ describe("Node Debugger", function() {
     it("should be at 0.1.3", function() {
       ndb.version.should.equal("0.1.3");
     });
+  });
 
-    it("should have verbose off by default", function() {
-      ndb.reset();
-      ndb.verbose.should.equal(false);
-    });
+  it("should have verbose off by default", function() {
+    ndb.reset();
+    ndb.verbose.should.equal(false);
   });
 });
