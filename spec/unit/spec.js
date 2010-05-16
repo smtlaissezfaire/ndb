e2793e88248efb9ffd5afb05022df149dcabcd10
Scott Taylor <scott@railsnewbie.com>
2010-05-16 18:23:52 -0400

    close ditz issue 13: wait until remote host is up to start the debugger instead of failing immediately.

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -8,7 +8,11 @@ describe('NodeDebugger', function() {
       ndb.commandCenter = command_center;
     });
 
-    describe("starting", function() {
+    describe("starting the debugger itself", function() {
+      before_each(function() {
+        connection = {};
+      });
+
       it("should output welcome text", function() {
         var text = "";
 
@@ -16,7 +20,7 @@ describe('NodeDebugger', function() {
           text += t;
         };
 
-        ndb.start();
+        ndb.startDebugger(connection);
 
         text.search(/welcome to the node debugger!/).should.not.equal(-1);
       });
@@ -28,11 +32,31 @@ describe('NodeDebugger', function() {
           text += t;
         };
 
-        ndb.start();
+        ndb.startDebugger(connection);
 
         text.search(/ndb> /).should.not.equal(-1);
       });
 
+      it("should set the command center's connection", function() {
+        ndb.startDebugger(connection);
+
+        command_center.connection.should.equal(connection);
+      });
+
+      it("should call the commandCenter's loop method", function() {
+        var loop_called = false;
+
+        command_center.loop = function() {
+          loop_called = true;
+        };
+
+        ndb.startDebugger(connection);
+
+        loop_called.should.be(true);
+      });
+    });
+
+    describe("starting", function() {
       it("should establish the connection on port 5858 and 127.0.0.1", function() {
         var port_received = null,
             host_received = null;
@@ -73,24 +97,6 @@ describe('NodeDebugger', function() {
         encoding_received.should.equal("ascii");
       });
 
-      it("should set the command center's connection", function() {
-        ndb.start();
-
-        command_center.connection.should.equal(connection);
-      });
-
-      it("should call the commandCenter's loop method", function() {
-        var loop_called = false;
-
-        command_center.loop = function() {
-          loop_called = true;
-        };
-
-        ndb.start();
-
-        loop_called.should.be(true);
-      });
-
       it("should install the connection's event listener", function() {
         var called = false;
 
@@ -116,6 +122,58 @@ describe('NodeDebugger', function() {
 
         args[0].should.equal("end");
         args[1].should.equal(ndb.Helpers.exit);
+      });
+
+      it("should listen for a connection error message", function() {
+        var args = [];
+
+        connection.addListener = function(msg, fun) {
+          if (msg === "error") {
+            args.push(msg);
+            args.push(fun);
+          }
+        };
+
+        ndb.start();
+
+        args[0].should.equal("error");
+        args[1].should.equal(ndb.restart);
+      });
+
+      describe("when there is an error", function() {
+        before_each(function() {
+          spy.stub(ndb.Helpers, "setTimeout");
+        });
+
+        it("should output that it is about to restart", function() {
+          spy.spyOn(ndb.Helpers, function() {
+            ndb.restart();
+
+            spy.intercepted(ndb.Helpers, "puts", function(text) {
+              text.should.equal("Could not connect to host 127.0.0.1 on port 5858.  Will try again in 2 seconds.");
+            });
+          });
+        });
+
+        it("should use the correct host + port in the error message", function() {
+          ndb.host = "localhost";
+          ndb.port = "2020";
+
+          spy.spyOn(ndb.Helpers, function() {
+            ndb.restart();
+
+            spy.intercepted(ndb.Helpers, "puts", function(text) {
+              text.should.equal("Could not connect to host localhost on port 2020.  Will try again in 2 seconds.");
+            });
+          });
+        });
+
+        it("should call start again in 2 seconds", function() {
+          spy.spyOn(ndb.Helpers, function() {
+            ndb.restart();
+            spy.intercepted(ndb.Helpers, "setTimeout", ndb.start, 2000);
+          });
+        });
       });
     });
   });
